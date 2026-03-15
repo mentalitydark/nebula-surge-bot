42a51e0526806c5724b8405e22eee9a41262aa88
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-03-15 11:09:50 -0300

    feat: add new env variables

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,10 @@
-import "./constants.js";
 import { validateEnv } from "@constatic/base";
 import { z } from "zod";
+import "./constants.js";
 
 export const env = await validateEnv(z.looseObject({
     BOT_TOKEN: z.string("Discord Bot Token is required").min(1),
     WEBHOOK_LOGS_URL: z.url().optional(),
-    GUILD_ID: z.string().optional()
+    GUILD_ID: z.string().optional(),
+    DATABASE_NAME: z.string().default("database.db")
 }));
