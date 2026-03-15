dab608477a692be1f4c91db5b85000c8a1f691cc
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2026-03-15 11:25:22 -0300

    feat: create table command permissions (#1)
    * feat: create new scripts
    
    * feat: create class Logger
    
    * feat: create data source
    
    * feat: create migration Create Table CommandPermission
    
    * feat: add dataSource initialize

---
+++ src/database/migrations/1773546922374-CommandPermission.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CommandPermission1773546922374 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: "command_permission",
+          columns: [
+            {
+              name: "id",
+              type: "integer",
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: "increment"
+            },
+            {
+              name: "command",
+              type: "varchar",
+              isNullable: false
+            },
+            {
+              name: "role_id",
+              type: "varchar",
+              isNullable: false
+            }
+          ],
+          indices: [
+            {
+              columnNames: ["command", "role_id"],
+              isUnique: true
+            },
+            {
+              columnNames: ["command"]
+            },
+            {
+              columnNames: ["role_id"]
+            }
+          ]
+        })
+      )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable("command_permission")
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { dataSource } from "#database";
 import { env } from "#env";
+import { Logger } from "#functions";
 import { bootstrap } from "@constatic/base";
+
+await dataSource.initialize();
+Logger.green('Database initialized!')
 
 await bootstrap({ meta: import.meta, env });
