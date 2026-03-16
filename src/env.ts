26426df87085f1d38e70e50b9e56bfeca1a7e34e
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-03-15 21:59:44 -0300

    feat: add NODE_ENV

--- src/env.ts
+++ src/env.ts
@@ -6,5 +6,6 @@ export const env = await validateEnv(z.looseObject({
     BOT_TOKEN: z.string("Discord Bot Token is required").min(1),
     WEBHOOK_LOGS_URL: z.url().optional(),
     GUILD_ID: z.string().optional(),
-    DATABASE_NAME: z.string().default("database.db")
+    DATABASE_NAME: z.string().default("database.db"),
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
 }));
