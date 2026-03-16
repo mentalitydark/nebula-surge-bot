32864c72d99a57bff59c2583283f69c4254ede49
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-03-15 21:14:41 -0300

    feat: create functions onError to commands and responders

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -1,3 +1,33 @@
 import { setupCreators } from "@constatic/base";
+import { createEmbed } from "@magicyan/discord";
 
-export const { createCommand, createEvent, createResponder } = setupCreators();
+const setup = setupCreators({
+  commands: {
+    async onError(error: any, interaction) {
+      await interaction.reply({
+        flags: ["Ephemeral"],
+        embeds: [
+          createEmbed({
+            description: error?.message || 'Tivemos um erro inesperado. Por favor, tente novamente mais tarde.',
+            color: constants.colors.danger
+          })
+        ]
+      })
+    }
+  },
+  responders: {
+    async onError(error: any, interaction) {
+      await interaction.reply({
+        flags: ["Ephemeral"],
+        embeds: [
+          createEmbed({
+            description: error?.message || 'Tivemos um erro inesperado. Por favor, tente novamente mais tarde.',
+            color: constants.colors.danger
+          })
+        ]
+      })
+    }
+  }
+})
+
+export const { createCommand, createEvent, createResponder } = setup;
