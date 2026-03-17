7dfe23eb1425cde1941b95e4b9be44d4c537da38
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-03-17 01:08:29 -0300

    feat: create folder entities in domain

---
+++ src/domain/entities/Builds.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export interface BuildsModel {
+  id: number;
+  equipament: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date|null;
+}
+
+@Entity({ name: "builds" })
+export class Builds implements BuildsModel {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({ type: "text", unique: true })
+    equipament: string;
+
+    @Column({ type: "text" })
+    content: string;
+
+    @CreateDateColumn({ name: "created_at" })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: "updated_at", nullable: true, default: null })
+    updatedAt: Date|null;
+}
