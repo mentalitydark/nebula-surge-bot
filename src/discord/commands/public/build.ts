65fe41e976c2f97a5c621aa00dd52bbb7f00a815
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-03-17 20:52:01 -0300

    feat: add pagination in command build

--- src/discord/commands/public/build.ts
+++ src/discord/commands/public/build.ts
@@ -1,11 +1,16 @@
 import { createCommand } from "#base";
+import { BuildsRepositoryInterface } from "#domain/repositories/BuildsRepositoryInterface.js";
+import { BuildsModel } from "#entities";
+import { GenerateButtonPrevNext } from "#functions";
 import { BuildsTypeormRepository } from "#repositories";
-import { brBuilder, createEmbed } from "@magicyan/discord";
-import { ApplicationCommandOptionType, ApplicationCommandType } from "discord.js";
+import { brBuilder, createEmbed, createRow } from "@magicyan/discord";
+import { ApplicationCommandOptionType, ApplicationCommandType, ButtonInteraction, InteractionCollector } from "discord.js";
+
+const PER_PAGE = 5;
 
 createCommand({
     name: "build",
-    description: "Pesquise por builds de equipamentos (warframe, armas, companheiro, etc)",
+    description: "Pesquise por builds de equipamentos feitas pelo clã Nebula Surge",
     type: ApplicationCommandType.ChatInput,
     options: [{
       name: "equipamento",
@@ -15,14 +20,14 @@ createCommand({
     }],
     async run(interaction) {
         const { options } = interaction
-
+        const repository = new BuildsTypeormRepository()
         const equipament = options.getString("equipamento", true)
+        
+        let currentPage = 1;
 
-        const repository =  new BuildsTypeormRepository()
+        const searchResult = await search(repository, equipament, currentPage)
 
-        const result = await repository.search({ filter: equipament })
-
-        if (result.total === 0) {
+        if (searchResult.total === 0) {
           await interaction.reply({
             flags: ["Ephemeral"],
             embeds: [
@@ -39,17 +44,49 @@ createCommand({
           return
         }
 
-        await interaction.reply({
+        const totalPages = Math.ceil(searchResult.total / PER_PAGE);
+
+        const response = await interaction.reply({
           flags: ["Ephemeral"],
-          embeds: result.data.slice(0, 10).map((build, index) => createEmbed({
-            color: constants.colors.primary,
-            description: brBuilder(
-              `### Build \`${build.equipament}\``,
-              build.content
-            ),
-            footer: `Build ${index + 1} de ${result.total}`,
-            timestamp: build.updatedAt ?? build.createdAt
-          }))
+          withResponse: true,
+          embeds: searchResult.data.map((build, index) => createEmbedBuild(build, index+1, searchResult.total, currentPage)),
+          components: [generateButtons(currentPage, totalPages)]
         })
+
+        const collector = response.resource?.message?.createMessageComponentCollector({ time: 120_000 }) as InteractionCollector<ButtonInteraction<"cached">>
+
+        collector.on('collect', async (i) => {
+          if (i.customId === 'prev' && currentPage > 1) currentPage--;
+          if (i.customId === 'next') currentPage++;
+
+          const searchResult = await search(repository, equipament, currentPage)
+
+          await i.update({
+            embeds: searchResult.data.map((build, index) => createEmbedBuild(build, index+1, searchResult.total, currentPage)),
+            components: [generateButtons(currentPage, totalPages)]
+          });
+        });
+
+        collector.on('end', async () => {
+          await interaction.editReply({ components: [] }).catch(() => {});
+        });
     }
 });
+
+function createEmbedBuild(build: BuildsModel, index: number, total: number, currentPage: number) {
+  return createEmbed({
+    color: constants.colors.primary,
+    title: build.equipament,
+    description: build.content,
+    footer: `Build ${(currentPage - 1) * PER_PAGE + index} de ${total}`,
+    timestamp: build.updatedAt ?? build.createdAt
+  })
+}
+
+function generateButtons(currentPage: number, totalPages: number) {
+  return createRow(GenerateButtonPrevNext(currentPage <= 1, currentPage >= totalPages));
+}
+
+async function search(repository: BuildsRepositoryInterface, equipament: string, page: number) {
+  return await repository.search({ filter: equipament, page, per_page: PER_PAGE })
+}
