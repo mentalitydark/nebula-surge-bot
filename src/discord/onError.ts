109a24d679c5074dd6c1893aa75905771ab76ff7
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-04-12 08:30:17 -0300

    refactor: simplifica o tratamento de erros na função onError para usar apenas AppError

--- src/discord/onError.ts
+++ src/discord/onError.ts
@@ -1,4 +1,4 @@
-import { BadRequestError, ConflictError, Forbidden, NotFoundError } from "#errors"
+import { AppError } from "#errors"
 import { Logger } from "#functions"
 import { createEmbed } from "@magicyan/discord"
 import { CacheType, CommandInteraction, MessageComponentInteraction, ModalSubmitInteraction } from "discord.js"
@@ -10,22 +10,11 @@ export async function onError(error: any, interaction: CommandInteraction<CacheT
     color: constants.colors.danger,
   }
 
-  if (ConflictError.isAppError(error)) {
-    embed.title = 'Erro de conflito'
-    embed.description = error.message
-  } else if (NotFoundError.isAppError(error)) {
-    embed.title = 'Recurso não encontrado'
-    embed.description = error.message
-  } else if (BadRequestError.isAppError(error)) {
-    embed.title = 'Requisição inválida'
-    embed.description = error.message
-  } else if (Forbidden.isAppError(error)) {
-    embed.title = 'Acesso negado'
-    embed.description = error.message
-  } else if (error instanceof Error) {
+  if (error instanceof AppError) {
+    embed.title = error.errorName
     embed.description = error.message
   } else {
-    console.error(error)
+    console.log(error)
   }
 
   Logger.error(String(embed.description))
