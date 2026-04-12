5fa24b87f8ae59272695546712e7747654736c8c
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-04-12 15:36:23 -0300

    feat: unifica comandos relacionados a build em um único comando

---
+++ src/discord/commands/build/update.ts
@@ -0,0 +1,50 @@
+import { FindBuildByIdUseCase } from '#application/use-cases/build/FindBuildByIdUseCase.js'
+import { requirePermissionDecorator } from '#functions'
+import { BuildsTypeormRepository } from '#repositories'
+import { createLabel, createModalFields, createTextInput } from '@magicyan/discord'
+import { ApplicationCommandOptionType, TextInputStyle } from 'discord.js'
+import command from './build.js'
+
+command.subcommand({
+  name: 'update',
+  description: 'Atualiza uma build existente',
+  options: [{
+    name: 'id',
+    description: 'ID da build a ser atualizada',
+    type: ApplicationCommandOptionType.Integer,
+    required: true
+  }],
+  run: requirePermissionDecorator(async (interaction) => {
+    const { options } = interaction
+    const id = options.getInteger("id", true)
+
+    const repository = new BuildsTypeormRepository()
+    const useCase = new FindBuildByIdUseCase(repository)
+
+    const build = await useCase.execute(id)
+
+    const nameInput = createTextInput({
+      customId: 'equipament',
+      required: true,
+      placeholder: 'Digite um ID único. Ex: Saryn Prime [Nuke] [End-Game]',
+      value: build.equipament
+    })
+
+    const contentInput = createTextInput({
+      customId: 'content',
+      required: true,
+      placeholder: 'Uma mensagem que irá disponibilizar o link do Overframe da builda. Permite markdown',
+      style: TextInputStyle.Paragraph,
+      value: build.content
+    })
+
+    await interaction.showModal({
+      customId: `/form/update-build/${build.id}`,
+      title: 'Atualizar build',
+      components: createModalFields(
+        createLabel("Identificador da build", nameInput),
+        createLabel("Conteúdo da mensagem", contentInput)
+      )
+    })
+  })
+})
