a1c2163f3a97b952e09ac57b8321e4171b944157
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-06-27 14:42:59 -0300

    feat: sobrescreve funções createCommand e subcommad

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -10,14 +10,33 @@ const { createCommand: originalCreateCommand, createEvent, createResponder } = s
 
 export const registeredCommands: Map<string, string> = new Map();
 
-const createCommand: typeof originalCreateCommand = (data) => {
-  if (Array.isArray(data.defaultMemberPermissions) && !data.defaultMemberPermissions.includes("Administrator")) {
-    registeredCommands.set(data.name, data.name);
-  } else if (!Array.isArray(data.defaultMemberPermissions)) {
-    registeredCommands.set(data.name, data.name);
+const createCommand: typeof originalCreateCommand = (commandData) => {
+  const hasRunFunction = !!commandData.run && typeof commandData.run === 'function';
+  const withoutDefaultPermissions = !Array.isArray(commandData.defaultMemberPermissions)
+
+  if (hasRunFunction && commandData.run!.name === 'requirePermission') {
+    registeredCommands.set(commandData.name, commandData.name)
+  } else if (hasRunFunction && withoutDefaultPermissions) {
+    registeredCommands.set(commandData.name, commandData.name)
   }
 
-  return originalCreateCommand({...data, dmPermission: false})
+  const command = originalCreateCommand(commandData);
+  const originalSubcommand = command.subcommand;
+
+  Object.defineProperty(command, "subcommand", {
+    value: function (subcommandData: Parameters<typeof command['subcommand']>[0]) {
+      const hasRunFunction = !!subcommandData.run && typeof subcommandData.run === 'function';
+
+      if (hasRunFunction && subcommandData.run!.name === 'requirePermission') {
+        registeredCommands.set(`${commandData.name}/${subcommandData.name}`, `${commandData.name}/${subcommandData.name}`)
+      }
+
+      return originalSubcommand.call(this, subcommandData);
+    }
+  })
+
+  return command
 }
 
 export { createCommand, createEvent, createResponder };
+
