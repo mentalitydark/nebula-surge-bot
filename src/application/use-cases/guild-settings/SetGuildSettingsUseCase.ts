b3e6a03f406cbbb0ebecf114936f7df9f55840b6
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-07-01 18:42:33 -0300

    feat: remove casos de uso obsoletos e adiciona novo caso de uso para configuração de guilda

---
+++ src/application/use-cases/guild-settings/SetGuildSettingsUseCase.ts
@@ -0,0 +1,45 @@
+import { GuildSettingsRepositoryInterface } from "#application/repositories/GuildSettingsRepositoryInterface.js";
+import { GuildSettings, GuildSettingsKeys, Settings } from "#entities";
+import { NotFoundError } from "#errors";
+
+type SetGuildSettingsProps = { [key in GuildSettingsKeys]?: string };
+
+export class SetGuildSettingsUseCase {
+  public constructor(
+    private readonly repository: GuildSettingsRepositoryInterface
+  ) { }
+
+  /** @throws {Error} */
+  public async execute(guildId: string, props: SetGuildSettingsProps): Promise<GuildSettings> {
+    try {
+      const guildSettings = await this.repository.findByGuild(guildId)
+
+      return this.updateGuildSettings(guildSettings, props)
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        return this.insertGuildSettings(guildId, props)
+      }
+
+      throw error
+    }
+  }
+
+  private async insertGuildSettings(guildId: string, props: SetGuildSettingsProps): Promise<GuildSettings> {
+    const guildSettings = this.repository.create({ guild: guildId, settings: Settings.fromJSON(props) })
+
+    const insertedGuildSettings = await this.repository.insert(guildSettings)
+
+    return insertedGuildSettings
+  }
+
+  private async updateGuildSettings(guildSettings: GuildSettings, props: SetGuildSettingsProps): Promise<GuildSettings> {
+    for (const [key, value] of Object.entries(props)) {
+      if (value !== undefined) {
+        guildSettings.settings?.set(key as GuildSettingsKeys, value)
+      }
+    }
+
+    return this.repository.update(guildSettings)
+  }
+
+}
