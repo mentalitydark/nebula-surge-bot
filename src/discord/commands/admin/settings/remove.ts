ba7fcd751035bc6b4f429875337b1521c8f765b8
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-07-01 18:58:07 -0300

    feat: adiciona subcomando 'remove' para excluir configurações do bot

---
+++ src/discord/commands/admin/settings/remove.ts
@@ -0,0 +1,49 @@
+import { SetGuildSettingsUseCase } from "#application/use-cases/guild-settings/SetGuildSettingsUseCase.js"
+import { GuildSettingsKeys, Settings } from "#domain/entities/Settings.js"
+import { BadRequestError } from "#errors"
+import { GuildSettingsTypeormRepository } from "#repositories"
+import { createEmbed } from "@magicyan/discord"
+import { ApplicationCommandOptionType } from "discord.js"
+import command from "./settings.js"
+
+command.subcommand({
+  name: 'remove',
+  description: 'Remove o valor de uma configuração do bot',
+  options: [{
+    name: 'setting',
+    description: 'Configuração que deseja remover',
+    type: ApplicationCommandOptionType.String,
+    required: true,
+    autocomplete: true
+  }],
+  async run(interaction) {
+    await interaction.deferReply({ ephemeral: true })
+
+    const settingOption = interaction.options.getString('setting', true) as GuildSettingsKeys
+
+    const guild = interaction.guild
+
+    if (!guild) {
+      throw new BadRequestError('Guild não encontrada')
+    }
+
+    if (!Settings.isValidKey(settingOption)) {
+      throw new BadRequestError('Configuração inválida')
+    }
+
+    const repository = new GuildSettingsTypeormRepository()
+    const useCase = new SetGuildSettingsUseCase(repository)
+
+    await useCase.execute(guild.id, { [settingOption]: null })
+
+    await interaction.editReply({
+      embeds: [
+        createEmbed({
+          title: 'Configuração removida',
+          description: `A configuração **${Settings.getDescription(settingOption)}** foi removida`,
+          color: constants.colors.success
+        })
+      ]
+    })
+  }
+})
