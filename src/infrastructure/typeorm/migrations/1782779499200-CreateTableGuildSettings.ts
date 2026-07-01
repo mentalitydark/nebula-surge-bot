dcd40be2b1b29e62f13b507da7df32048dbd161e
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-06-30 22:50:50 -0300

    feat: cria migração para a tabela de configurações de guilda

---
+++ src/infrastructure/typeorm/migrations/1782779499200-CreateTableGuildSettings.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateTableGuildSettings1782779499200 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "guild_settings",
+            columns: [{
+                name: "id",
+                type: "integer",
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: "increment"
+            }, {
+                name: "guild",
+                type: "varchar",
+                isNullable: false
+            }, {
+                name: "settings",
+                type: "jsonb",
+                isNullable: true
+            }, {
+                name: "created_at",
+                type: "timestamp",
+                default: "now()"
+            }, {
+                name: "updated_at",
+                type: "timestamp",
+                default: "now()",
+                onUpdate: "now()"
+            }],
+            indices: [{
+                name: "IDX_GUILD_SETTINGS_GUILD",
+                columnNames: ["guild"],
+                isUnique: true
+            }]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("guild_settings");
+    }
+
+}
