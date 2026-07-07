f5ead4920d8f228b5dd63104137594a8af577647
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-07-07 13:06:19 -0300

    feat: integra cache em memória no caso de uso SearchCommandPermissions

--- src/application/use-cases/command-permission/SearchCommandPermissionsUseCase.ts
+++ src/application/use-cases/command-permission/SearchCommandPermissionsUseCase.ts
@@ -1,15 +1,38 @@
-import { SearchCommandPermissionInput } from "#application/repositories/CommandPermissionRepositoryInterface.js";
-import { CommandPermissionRepositoryInterface } from "#application/repositories/CommandPermissionRepositoryInterface.js";
+import { CacheProviderInterface } from "#application/providers/CacheProviderInterface.js";
+import { CommandPermissionRepositoryInterface, SearchCommandPermissionInput } from "#application/repositories/CommandPermissionRepositoryInterface.js";
 import { SearchOutput } from "#application/repositories/RepositoryInterface.js";
 import { CommandPermissionModel } from "#entities";
 
 export class SearchCommandPermissionsUseCase {
   public constructor(
     private readonly repository: CommandPermissionRepositoryInterface,
-  ) {}
+    private readonly cache: CacheProviderInterface<CommandPermissionModel[]>
+  ) { }
 
   public async execute(searchProps: SearchCommandPermissionInput): Promise<SearchOutput<CommandPermissionModel>> {
+    const filters = {
+      guild: searchProps.guild,
+      page: searchProps.page ?? 1,
+      per_page: searchProps.per_page ?? 10,
+      filter: searchProps.filter ?? 'none'
+    }
+
+    const key = JSON.stringify(filters);
+
+    const cached = this.cache.get(key);
+
+    if (cached) {
+      return {
+        current_page: filters.page,
+        per_page: filters.per_page,
+        data: cached,
+        total: cached.length
+      };
+    }
+
     const response = await this.repository.search(searchProps);
+
+    this.cache.set(key, response.data, 10 * 60);
 
     return response;
   }
