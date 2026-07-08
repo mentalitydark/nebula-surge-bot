e45ee1dc2bc7d311b6fe2cb3fb665c29ac1feed9
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-07-07 22:55:41 -0300

    feat: adiciona documentação para o método set no CacheProviderInterface

--- src/application/providers/CacheProviderInterface.ts
+++ src/application/providers/CacheProviderInterface.ts
@@ -1,5 +1,11 @@
 export interface CacheProviderInterface<T> {
   get(key: string | number): T | null
+  /**
+   * @param {string | number} key - The key to store the value under.
+   * @param {T} value - The value to store.
+   * @param {number} [ttl] - The time to live in seconds. If not provided, the value will be stored indefinitely.
+   * @returns {void}
+   */
   set(key: string | number, value: T, ttl?: number): void
   delete(key: string | number): void
   clear(): void
