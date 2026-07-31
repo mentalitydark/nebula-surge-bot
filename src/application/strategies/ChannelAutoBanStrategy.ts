a2f63ba52e96b38fc6bee26f61638ad112709f8e
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-07-30 21:26:29 -0300

    refactor: remove estratégias de configuração de canal obsoletas

--- src/application/strategies/ChannelAutoBanStrategy.ts
+++
@@ -1,7 +0,0 @@
-import { GuildSettingsKeys } from "#entities";
-import { ChannelSettingStrategy } from "./ChannelSettingStrategy.js";
-
-export class ChannelAutoBanStrategy extends ChannelSettingStrategy {
-  public readonly key = GuildSettingsKeys.CHANNEL_AUTO_BAN;
-  public readonly allowMultipleChannels = true;
-}
--- src/application/strategies/ChannelAutoBanVoteStrategy.ts
+++
@@ -1,7 +0,0 @@
-import { GuildSettingsKeys } from "#entities";
-import { ChannelSettingStrategy } from "./ChannelSettingStrategy.js";
-
-export class ChannelAutoBanVoteStrategy extends ChannelSettingStrategy {
-  public readonly key = GuildSettingsKeys.CHANNEL_AUTO_BAN_VOTE;
-  public readonly allowMultipleChannels = false;
-}
