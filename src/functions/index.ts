c0b8ea66fb720ecc14597be223164093ee3c5dbf
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-08-06 23:46:40 -0300

    refactor: remover funções e utilitários obsoletos

--- src/functions/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './DiscordUtils.js';
-export * from './GenerateButtonPrevNext.js';
-export * from './Logger.js';
-export * from './requirePermissionDecorator.js';
-export * from './suppress.js';
-
