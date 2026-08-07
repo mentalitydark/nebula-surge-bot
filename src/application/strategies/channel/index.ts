869af140b5f4424609716740e2a052f608b4b6b8
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-08-06 23:45:40 -0300

    refactor: remove estratégias de configuração obsoletas

--- src/application/strategies/channel/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './ChannelAutoBanStrategy.js'
-export * from './ChannelAutoBanVoteStrategy.js'
-export * from './ChannelLogsStrategy.js'
-export * from './ChannelMessagesRemovedStrategy.js'
-export * from './ChannelSettingStrategy.js'
--- src/application/strategies/mixed/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './AutoBanVoteThresholdSettingStrategy.js';
-export * from './MixedSettingStrategy.js';
-
