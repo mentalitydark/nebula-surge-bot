b5df9e541ea4561ba2abf0f4f79b1cc4b7120716
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-08-09 23:28:26 -0300

    fix: refatora a lógica de extração do usuário no NotBotMiddleware para melhor legibilidade

--- src/presentation/middlewares/NotBotMiddleware.ts
+++ src/presentation/middlewares/NotBotMiddleware.ts
@@ -16,29 +16,32 @@ export const NotBotMiddleware: GuardFunction<
   | UserSelectMenuInteraction
   | SimpleCommandMessage
 > = async (arg, client, next) => {
-  const argObj = arg instanceof Array ? arg[0] : arg;
+  const args = arg instanceof Array ? arg : [arg];
+  const argObj = args[0];
+  const secondary = args[1];
   const user =
-    argObj instanceof CommandInteraction
-      ? argObj.user
-      : argObj instanceof MessageReaction
-        ? argObj.message.author
-        : argObj instanceof VoiceState
-          ? argObj.member?.user
-          : argObj instanceof Message
-            ? argObj.author
-            : argObj instanceof SimpleCommandMessage
-              ? argObj.message.author
-              : argObj instanceof ButtonInteraction ||
-                argObj instanceof ChannelSelectMenuInteraction ||
-                argObj instanceof CommandInteraction ||
-                argObj instanceof ContextMenuCommandInteraction ||
-                argObj instanceof MentionableSelectMenuInteraction ||
-                argObj instanceof ModalSubmitInteraction ||
-                argObj instanceof RoleSelectMenuInteraction ||
-                argObj instanceof StringSelectMenuInteraction ||
-                argObj instanceof UserSelectMenuInteraction
-                ? argObj.member?.user
-                : argObj.message.author;
+    typeof secondary === "object" && secondary !== null && "bot" in secondary
+      ? (secondary as { bot: boolean })
+      : argObj instanceof CommandInteraction
+        ? argObj.user
+        : argObj instanceof MessageReaction
+          ? argObj.message.author
+          : argObj instanceof VoiceState
+            ? argObj.member?.user
+            : argObj instanceof Message
+              ? argObj.author
+              : argObj instanceof SimpleCommandMessage
+                ? argObj.message.author
+                : argObj instanceof ButtonInteraction ||
+                  argObj instanceof ChannelSelectMenuInteraction ||
+                  argObj instanceof ContextMenuCommandInteraction ||
+                  argObj instanceof MentionableSelectMenuInteraction ||
+                  argObj instanceof ModalSubmitInteraction ||
+                  argObj instanceof RoleSelectMenuInteraction ||
+                  argObj instanceof StringSelectMenuInteraction ||
+                  argObj instanceof UserSelectMenuInteraction
+                  ? argObj.member?.user
+                  : undefined;
 
   if (!user?.bot) {
     await next();
