2d6311e31de3dcca76e0088a57593ba9ec29e0db
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-08-14 20:52:19 -0300

    feat: adiciona exceção ForbiddenException para controle de acesso

---
+++ src/domain/errors/ForbiddenException.ts
@@ -0,0 +1,8 @@
+import { Exception } from "./Exception";
+
+export class ForbiddenException extends Exception {
+  public constructor() {
+    super('Acesso negado: você não tem permissão para executar esta ação.');
+    this.name = 'ForbiddenException';
+  }
+}
