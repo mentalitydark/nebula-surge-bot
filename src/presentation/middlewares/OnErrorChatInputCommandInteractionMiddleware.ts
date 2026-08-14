5835eef08338459c85077ca532aeae9b107304ac
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-08-14 20:56:46 -0300

    feat: adiciona middleware OnError para tratamento de erros em comandos de interação

---
+++ src/presentation/middlewares/OnErrorChatInputCommandInteractionMiddleware.ts
@@ -0,0 +1,42 @@
+import { createEmbed } from "@magicyan/discord";
+import { ChatInputCommandInteraction } from "discord.js";
+import { GuardFunction } from "discordx";
+import { colors } from "../constants";
+import { Exception } from "@/domain/errors";
+
+export const OnErrorChatInputCommandInteractionMiddleware: GuardFunction<ChatInputCommandInteraction> = async (interaction, client, next) => {
+  try {
+    await next();
+  } catch (error) {
+    if (!(interaction instanceof ChatInputCommandInteraction)) {
+      throw error
+    }
+
+    const isException = error instanceof Exception;
+    const isReplyOrDeferred = interaction.replied || interaction.deferred;
+    const method = isReplyOrDeferred ? "followUp" : "reply";
+
+    if (isException) {
+      await interaction[method]({
+        flags: ['Ephemeral'],
+        embeds: [createEmbed({
+          title: "Erro",
+          description: error.message,
+          color: colors.danger
+        })]
+      });
+      return;
+    }
+
+    await interaction[method]({
+      flags: ['Ephemeral'],
+      embeds: [createEmbed({
+        title: "Erro",
+        description: "Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.",
+        color: colors.danger
+      })]
+    });
+
+    throw error;
+  }
+}
