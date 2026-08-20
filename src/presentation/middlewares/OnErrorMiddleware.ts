7736124fdc68ccdd35e32be4170ee70cdad97791
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-08-20 19:32:54 -0300

    refactor: atualiza OnErrorMiddleware para usar injeção de dependência com tsyringe

--- src/presentation/middlewares/OnErrorMiddleware.ts
+++ src/presentation/middlewares/OnErrorMiddleware.ts
@@ -1,13 +1,16 @@
-import { ConsoleLoggerProvider } from "@/infrastructure/providers";
+import { container } from "tsyringe";
 import { GuardFunction } from "discordx";
-
-const logger = new ConsoleLoggerProvider(console);
+import { TOKENS } from "@/infrastructure/container/tokens";
+import { LoggerProviderInterface } from "@/application/providers";
 
 export const OnErrorMiddleware: GuardFunction = async (_, __, next) => {
   try {
     await next();
   } catch (error) {
     const isError = error instanceof Error;
+
+    const logger = container.resolve<LoggerProviderInterface>(TOKENS.LoggerProviderInterface);
+
     logger.error(isError ? error : String(error));
   }
 }
