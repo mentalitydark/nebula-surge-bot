582a255d3481ccedc96b0d136638d8da6c9b4bf8
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-09-10 19:43:08 -0300

    feat: atualiza a injeção de dependência para usar DiscordLogInterface

--- src/application/use-cases/SendAuditLogUseCase.ts
+++ src/application/use-cases/SendAuditLogUseCase.ts
@@ -1,15 +1,15 @@
 import { inject, injectable } from 'tsyringe'
 
+import type { AuditLogDTO, DiscordLogInterface, UseCaseInterface } from '@/application/contracts'
+
 import { APPLICATION_TOKENS } from '@/application/container/tokens'
-import { type UseCaseInterface } from '@/application/contracts'
-import { type AuditLogDTO, type DiscordLogProviderInterface } from '@/application/providers'
 
 @injectable()
 export class SendAuditLogUseCase implements UseCaseInterface<AuditLogDTO, Promise<void>> {
 
   public constructor(
-    @inject(APPLICATION_TOKENS.DiscordLogProviderInterface)
-    private readonly auditLogProvider: DiscordLogProviderInterface
+    @inject(APPLICATION_TOKENS.DiscordLogInterface)
+    private readonly auditLogProvider: DiscordLogInterface
   ) { }
 
   public async execute(input: AuditLogDTO): Promise<void> {
