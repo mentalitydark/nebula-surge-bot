875dc1e1fb742277c30452d5e7bf6ce679f46f55
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-09-12 01:11:06 -0300

    feat: atualiza a classe SendAuditLogUseCase para usar DiscordLogDto em vez de AuditLogDTO

--- src/application/use-cases/SendAuditLogUseCase.ts
+++ src/application/use-cases/SendAuditLogUseCase.ts
@@ -1,18 +1,19 @@
 import { inject, injectable } from 'tsyringe'
 
-import type { AuditLogDTO, DiscordLogInterface, UseCaseInterface } from '@/application/contracts'
+import type { DiscordLogInterface, UseCaseInterface } from '@/application/contracts'
+import type { DiscordLogDto } from '@/application/dtos'
 
 import { APPLICATION_TOKENS } from '@/application/container/tokens'
 
 @injectable()
-export class SendAuditLogUseCase implements UseCaseInterface<AuditLogDTO, Promise<void>> {
+export class SendAuditLogUseCase implements UseCaseInterface<DiscordLogDto, Promise<void>> {
 
   public constructor(
     @inject(APPLICATION_TOKENS.DiscordLogInterface)
     private readonly auditLogProvider: DiscordLogInterface
   ) { }
 
-  public async execute(input: AuditLogDTO): Promise<void> {
+  public async execute(input: DiscordLogDto): Promise<void> {
     await this.auditLogProvider.sendLog(input)
   }
 
