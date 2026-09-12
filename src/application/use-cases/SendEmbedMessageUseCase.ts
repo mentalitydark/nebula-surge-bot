fefb5e05448f4255c4df2b5be1b99eabb27b2009
mentalitydark <guilherme.oliveira.01@outlook.com>
2026-09-12 01:11:17 -0300

    feat: atualiza SendEmbedMessageUseCase para usar SendEmbedMessageDto em vez de SendEmbedMessageDTO

--- src/application/use-cases/SendEmbedMessageUseCase.ts
+++ src/application/use-cases/SendEmbedMessageUseCase.ts
@@ -1,18 +1,19 @@
 import { inject, injectable } from 'tsyringe'
 
 import type { UseCaseInterface } from '@/application/contracts'
-import type { SendEmbedMessageDTO, SendEmbedMessageInterface } from '@/application/contracts/SendEmbedMessageInterface'
+import type { SendEmbedMessageInterface } from '@/application/contracts/SendEmbedMessageInterface'
+import type { SendEmbedMessageDto } from '@/application/dtos'
 
 import { APPLICATION_TOKENS } from '@/application/container/tokens'
 
 @injectable()
-export class SendEmbedMessageUseCase implements UseCaseInterface<SendEmbedMessageDTO, Promise<void>> {
+export class SendEmbedMessageUseCase implements UseCaseInterface<SendEmbedMessageDto, Promise<void>> {
   public constructor(
     @inject(APPLICATION_TOKENS.SendEmbedMessageInterface)
     private readonly provider: SendEmbedMessageInterface
   ) { }
 
-  public async execute(data: SendEmbedMessageDTO): Promise<void> {
+  public async execute(data: SendEmbedMessageDto): Promise<void> {
     await this.provider.send(data)
   }
 }
